cd16455f516987946278c710a11f38099a33df5a
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-08-25 10:54:34 +0600

    W0 First Hhour - String

--- w0.py
+++ w0.py
@@ -1,1 +1,33 @@
 print("CS50P Lecture 0 - Functions")
+
+# taking name as input and removing whitespace and capitalizing the first letter of the string
+#name = input("What is your name?").strip().title()
+
+# printing name as output
+#print("hello, " + name)
+#print("hello,", name)
+#print(f"hello, {name}")
+
+#  delete new line in print function by using end=""
+#print("hello, ", end=" ")
+#print(name)
+
+# speerate between print function by using sep=""
+#print("hello,", name, sep="...")
+
+# removing whitespace from str
+#name = name.strip()
+
+# capitalize the first letter of the string
+#name = name.capitalize()
+
+
+# capitalize the first letter of each word in the string
+#name = name.title()
+
+# combine all the above functions
+#name = name.strip().title()
+
+#split user name into first and last name and print it
+#first, last = name.split(" ")
+#print(f"hello, {first} {last}")
---
+++ w0_notes.md
@@ -0,0 +1,12 @@
+# CS50P "Week 0 - Functions" Notes
+
+variable_name = input (prompt to show/question)
+print("promt to show as output", variable_name)
+print("promt to show as output" + variable_name)
+
+[
+print("prompt to show as output", end="")
+print(variable_name)
+] ---> end="" will delete the new line of 2nd print. wahtever in the "" of end will be shown in between both print fuction
+
+print("pronpt to show as output", variable name, sep="") ---> sep="" will add whatever inside the "" in between both argument of the print function
