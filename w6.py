b0d28111cc32f127e360153174e5956692f1924f
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-08-29 20:32:19 +0600

    W6 is not done, time stamp 50:35

--- w6.py
+++ w6.py
@@ -27,19 +27,86 @@ with open("names.txt", "a") as file:
 '''
 
 # read
-
+'''
 with open("names.txt", "r") as file:
-    lines = file.readlines()
+    #lines = file.readlines()
+    for line in file:
+        print("hello,", line.rstrip())
 
-for line in lines:
+#for line in lines:
     #print("hello,", line)
-    print("hello,", line.rstrip())
+    #print("hello,", line.rstrip())
+'''
+'''
+names=[]
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+#for name in sorted(names):
+for name in sorted(names, reverse = True):
+    print(f"hello, {name}")
+'''
+'''
+with open("names.txt") as file:
+    for line in sorted(file):
+        print("hello,", line.rstrip())
+'''
 
 
+# CSV
+'''
+with open("students.csv") as file:
+    for line in file:
+        row = line.rstrip().split(",")
+        print(f"{row[0]} is in {row[1]}")
+'''
 
+'''
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
+'''
 
+'''
+students=[]
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        students.append(f"{name} is in {house}")
+for i in sorted(students):
+    print(i)
+'''
 
+'''
+students = []
 
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student ={}
+        #student["name"] = name
+        #student["house"] = house
+        student = {"name": name, "house": house}
+        students.append(student)
+
+for i in students:
+    print(f"{i['name']} is in {i['house']}")   
+'''
+
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        students.append({"name": name, "house": house})
+
+def get_name(student):
+    return student["name"]
+
+for i in sorted(students, key = get_name):
+    print(f"{i['name']} is in {i['house']}")
 
 
 
