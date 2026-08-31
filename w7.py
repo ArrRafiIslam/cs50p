9d0c94c24fed6efc5113f345b6320dd3ddc4a1e7
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-08-31 21:54:55 +0600

    W7 time stamp 29:12, regular expression automata

--- w7.py
+++ w7.py
@@ -1,1 +1,252 @@
-print("hello, world!")
+
+# Regular Expression
+'''
+email = input("Whats your email? ").strip()
+if "@" in email and "." in email :
+    print("Valid")
+else:
+    print("Invalid")
+'''
+'''
+email = input("Whats your email? ").strip()
+username, domain = email.split("@")
+
+if username and "." in domain:
+    print("valid")
+else:
+    print("Invalid")
+'''
+'''
+email = input("Whats your email? ").strip()
+username, domain = email.split("@")
+
+if username and domain.endswith(".edu"):
+    print("valid")
+else:
+    print("Invalid")
+'''
+# re library
+'''
+import re
+email = input("Whats your email? ").strip()
+#username, domain = email.split("@")
+
+#if username and domain.endswith(".edu"):
+if re.search("@", email):
+    print("valid")
+else:
+    print("Invalid")
+'''
+
+import re
+email = input("Whats your email? ").strip()
+
+#if re.search(".+@.+", email):
+#if re.search("..*@.+", email):
+if re.search(r".+@.+\.edu", email):
+    print("valid")
+else:
+    print("Invalid")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
