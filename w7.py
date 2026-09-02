58f49a97b10a0f83c191596cae93fd183f5c9d74
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-09-02 21:30:06 +0600

    clearning up user input finished

--- w7.py
+++ w7.py
@@ -106,14 +106,51 @@ if re.search(rf"^\\w+@\\w+\\.(com|net|edu|gov|org|io)$", email, re.IGNORECASE):
 else:
     print("Invalid")
 '''
-
+'''
 import re
 email = input("Whats your email? ").strip()
 
-if re.search(rf"^\w+@(\w+\.)?\w+\.(com|net|edu|gov|org|io)$", email, re.IGNORECASE):
+if re.search(rf"^\\w+@(\\w+\\.)?\\w+\\.(com|net|edu|gov|org|io)$", email, re.IGNORECASE):
     print("valid")
 else:
     print("Invalid")
+'''
+
+# clearning Up user input
+'''
+name = input("What's your name? ").strip()
+if "," in name:
+    last, first = name.split(", ")
+    name = f"{first} {last}"
+print(f"hello, {name}")
+'''
+'''
+import re
+name = input("What's your name? ").strip()
+matches = re.search(r"^(.+), (.+)$", name)
+if matches:
+    last, first =  matches.groups()
+    name = first + " " + last
+print(f"hello, {name}")
+'''
+'''
+import re
+name = input("What's your name? ").strip()
+#matches = re.search(r"^(.+), (.+)$", name)
+matches = re.search(r"^(.+), *(.+)$", name)
+
+if matches:
+    name = matches.group(2) + " " + matches.group(1)
+print(f"hello, {name}")
+'''
+
+import re
+name = input("What's your name? ").strip()
+#matches = re.search(r"^(.+), *(.+)$", name)
+
+if matches:= re.search(r"^(.+), *(.+)$", name):
+    name = matches.group(2) + " " + matches.group(1)
+print(f"hello, {name}")
 
 
 
