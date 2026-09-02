92ee731c2190a1e1bfe2cb2697147a393432e741
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-09-03 01:54:34 +0600

    w8 timestamp 59:17 -- working on ValueError

--- w8_student.py
+++ w8_student.py
@@ -104,8 +104,78 @@ if __name__ == "__main__":
 '''
 
 # Start of "CLASSES"
+#class is a custom detatype
+#when using class, it creates object/instance
+
+'''
+class Student:
+    ...
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")       #"name" and "house" are the "attributes"/"instance variables" of the class named "Student"
+
+def get_student():
+    student = Student()     #"student" is the "object"/"instance" of class named "Student"
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
+
+if __name__ == "__main__":
+    main()
+'''
+
+'''
+class Student:
+    #classes come with certain function/methods which are defineable and behave in a special way
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")       #"name" and "house" are the "attributes"/"instance variables" of the class named "Student"
+
+def get_student():
+    #student = Student()     #"student" is the "object"/"instance" of class named "Student"
+    name = input("Name: ")
+    house = input("House: ")
+    student= Student(name, house)       #Constuctor call # Each class creates a functuion with the same name
+    return student
+
+if __name__ == "__main__":
+    main()
+'''
+# raise
+
+from multiprocessing import Value
 
 
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    try:
+        return Student(name, house)
+    except ValueError:
+        ...
+
+if __name__ == "__main__":
+    main()
+
+# 59:17
 
 
 
