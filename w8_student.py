487bfc4fdc9a4d8fc388781222194dcaa96e3157
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-09-03 16:01:22 +0600

    w8 timestamp 1:17:59 -- working on Raise

--- w8_student.py
+++ w8_student.py
@@ -147,10 +147,7 @@ if __name__ == "__main__":
     main()
 '''
 # raise
-
-from multiprocessing import Value
-
-
+'''
 class Student:
     def __init__(self, name, house):
         if not name:
@@ -167,15 +164,107 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    try:
-        return Student(name, house)
-    except ValueError:
-        ...
+    return Student(name, house)
+
 
 if __name__ == "__main__":
     main()
+'''
 
-# 59:17
+'''
+class Student:
+    def __init__(self, name, house, patronus):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+        self.patronus = patronus
+    
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+def main():
+    student = get_student()
+    #print(f"{student.name} from {student.house}")
+    print(student)
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
+
+if __name__ == "__main__":
+    main()
+'''
+
+'''
+class Student:
+    def __init__(self, name, house, patronus):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+        self.patronus = patronus
+    
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🐴"
+            case "Otter":
+                return "🦦"
+            case "Jack Russell terrier":
+                return "🐶"
+            case _:
+                return "🪄"
+            
+
+def main():
+    student = get_student()
+    print("Expecto Patronum!")
+    #print(f"{student.name} from {student.house}")
+    print(student.charm())
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
+
+if __name__ == "__main__":
+    main()
+'''
+
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+    
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+def main():
+    student = get_student()
+    print(student)
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
 
 
 
