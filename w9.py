8c6ce887d859b836d85054cd053ca952f2fc4db2
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-09-06 23:53:39 +0600

    argprase complete

--- w9.py
+++ w9.py
@@ -130,7 +130,7 @@ print(meows, end="")
 '''
 
 #Docstrings
-
+'''
 def meow(n: int) -> str:
     """
     Meow n times.
@@ -146,14 +146,52 @@ number: int = int(input("number: "))
 meows: str = meow(number)
 print(meows, end="")
 
+'''
 
+# argprase
+'''
+import sys
 
+if len(sys.argv) == 1:
+    print("meow")
+elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+    n = int(sys.argv[2])
+    for _ in range(n):
+        print("meow")
+else:
+    print("usage: meows.py [-n NUMBER]")
+'''
+'''
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-n")
+args = parser.parse_args()
 
+for _ in range(int(args.n)):
+    print("meow")
 
+'''
+'''
+import argparse
 
+parser = argparse.ArgumentParser(description="Meow like a cat")
+parser.add_argument("-n", help="number of times to meow")
+args = parser.parse_args()
 
+for _ in range(int(args.n)):
+    print("meow")
+'''
+'''
+import argparse
 
+parser = argparse.ArgumentParser(description="Meow like a cat")
+parser.add_argument("-n", default=1, help="number of times to meow", type=int)
+args = parser.parse_args()
+
+for _ in range(args.n):
+    print("meow")
+'''
 
 
 
