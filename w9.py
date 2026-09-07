1cf747c483ea551b800cdde53d28feb1218ad5f6
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-09-07 22:55:32 +0600

    list comprehensions complete

--- w9.py
+++ w9.py
@@ -271,14 +271,99 @@ def f(*args, **kwargs):
 f(galleons=100, sickles=50, knuts=25)
 '''
 
+#map
+'''
+def main():
+    yell("This is CS50")
 
+def yell(word):
+    print(word.upper())
 
+if __name__ == "__main__":
+    main()
+'''
+'''
+def main():
+    yell(["This", "is", "CS50"])
 
+def yell(words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
 
+if __name__ == "__main__":
+    main()
+'''
+'''
+def main():
+    yell("This", "is", "CS50")
 
+def yell(*words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
 
+if __name__ == "__main__":
+    main()
+'''
+'''
+def main():
+    yell("This", "is", "CS50")
 
+def yell(*words):
+    uppercased = map(str.upper, words)
+    print(*uppercased)
 
+if __name__ == "__main__":
+    main()
+'''
+
+#List Comprehensions
+'''
+def main():
+    yell("This", "is", "CS50")
+
+def yell(*words):
+    uppercased = [arg.upper() for arg in words]
+    print(*uppercased)
+
+if __name__ == "__main__":
+    main()
+'''
+'''
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+]
+
+gryffindors = []
+for student in students:
+    if student["house"] == "Gryffindor":
+        gryffindors.append(student["name"])
+
+for gryffindor in sorted(gryffindors):
+    print(gryffindor)
+'''
+'''
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+]
+
+gryffindors = [
+    student["name"] for student in students if student["house"] == "Gryffindor"
+]
+
+for gryffindor in sorted(gryffindors):
+    print(gryffindor)
+
+'''
 
 
 
