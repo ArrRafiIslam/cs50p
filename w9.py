7a6876235710506bc7f9f88b1cd2f5955ca240bd
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-09-09 19:56:00 +0600

    CS50P d&d, all weeks notes will be uploaded later

--- w9.py
+++ w9.py
@@ -490,7 +490,16 @@ if __name__ == "__main__":
 '''
 
 
+#Fun
 
+import cowsay
+import pyttsx3
+
+engine = pyttsx3.init()
+this = input("What's this? ")
+cowsay.cow(this)
+engine.say(this)
+engine.runAndWait()
 
 
 
