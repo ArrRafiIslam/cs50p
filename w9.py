345e8d33a4356aa1b4da27208c8fa47f60ce4bcb
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-09-09 19:51:33 +0600

    Week 9 done, w9_ notes.md not done

--- w9.py
+++ w9.py
@@ -399,15 +399,95 @@ for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
 
 
 # Dictionary comprehensions
+'''
+students = ["Hermione", "Harry", "Ron"]
+gryffindors = []
 
+for student in students:
+    gryffindors.append({"name": student, "house": "Gryffindor"})
 
+print(gryffindors)
+'''
 
+'''
+students = ["Hermione", "Harry", "Ron"]
+gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+print(gryffindors)
+'''
+'''
+students = ["Hermione", "Harry", "Ron"]
+gryffindors = {student: "Gryffindor" for student in students}
+print(gryffindors)
+'''
 
+#Enumerate
+'''
+students = ["Hermione", "Harry", "Ron"]
+for i in range(len(students)):
+    print(i + 1, students[i])
+'''
+'''
+students = ["Hermione", "Harry", "Ron"]
+for i, student in enumerate(students):
+    print(i + 1, student)
+'''
 
+#Generators & Iterators
+'''
+n = int(input("What's n? "))
+for i in range(n):
+    print("🐑" * i)
+'''
+'''
+def main():
+    n = int(input("What's n? "))
+    for i in range(n):
+        print("🐑" * i)
 
+if __name__ == "__main__":
+    main()
+'''
+'''
+def main():
+    n = int(input("What's n? "))
+    for i in range(n):
+        print(sheep(i))
 
+def sheep(n):
+    return "🐑" * n
 
+if __name__ == "__main__":
+    main()
+'''
 
+'''
+def main():
+    n = int(input("What's n? "))
+    for s in sheep(n):
+        print(s)
+
+def sheep(n):
+    flock = []
+    for i in range(n):
+        flock.append("🐑" * i)
+    return flock
+
+if __name__ == "__main__":
+    main()
+'''
+'''
+def main():
+    n = int(input("What's n? "))
+    for s in sheep(n):
+        print(s)
+
+def sheep(n):
+    for i in range(n):
+        yield "🐑" * i
+
+if __name__ == "__main__":
+    main()
+'''
 
 
 
