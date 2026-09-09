d9edfe7c696483dc50d9be13e6555356f99ec1ea
Md Arr Rafi Islam <mdarrrafiislam@gmail.com>
2026-09-09 19:38:01 +0600

    filters complete

--- w9.py
+++ w9.py
@@ -366,6 +366,40 @@ for gryffindor in sorted(gryffindors):
 '''
 
 
+#Filter
+'''
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+]
+
+def is_gryffindor(s):
+    return s["house"] == "Gryffindor"
+
+gryffindors = filter(is_gryffindor, students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+    print(gryffindor["name"])
+'''
+'''
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+]
+
+gryffindors = filter(lambda s: s["house"] == "Gryffindor", students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+    print(gryffindor["name"])
+'''
+
+
+# Dictionary comprehensions
+
 
 
 
